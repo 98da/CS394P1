c6432d95fc940d5b44542aa69ca8f00d9b5adf27
ee977 <ege.eren.21293@ozu.edu.tr>
2021-12-08 18:06:05 +0300

    not finished
    step by step figuring it out

--- app/src/main/java/com/example/myapplication/loginPage.kt
+++ app/src/main/java/com/example/myapplication/loginPage.kt
@@ -5,9 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.myapplication.databinding.FragmentLoginPageBinding
 
 
 class loginPage : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginPageBinding>(inflater,R.layout.fragment_login_page,container,false)
+        binding.button.setOnClickListener{  view:View ->
+            view.findNavController().navigate(R.id.action_loginPage2_to_mainPage)
+
+        }
+        return binding.root
+    }
 
 }
